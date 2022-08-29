a6eab71178b5aa20d80b41669e590793342d1d00
Kevin <wcdma020@hotmail.com>
2022-08-30 01:50:15 +0800

    chore(Kevin): 调整权限不足时的提示信息

--- app/services/auth/AuthServices.php
+++ app/services/auth/AuthServices.php
@@ -50,7 +50,7 @@ class AuthServices extends BaseServices
             return $this->dao->queryMenu($groupRole, ['type' => 3])->toArray();
         }, 3600 * 24 * 7);
         if (!in_array($rules, array_column($roleMenu, 'routes'))) {
-            throw new AuthException("Access Denied! You don't have permission to access this resource", 403);
+            throw new AuthException("Access Denied. You don't have permission to access this resource", 403);
         }
     }
 
