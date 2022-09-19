13f04137ed30a36cbcbca59c31887a5cb7dae7e2
Kevin <wcdma020@hotmail.com>
2022-09-19 12:59:20 +0800

    chore(Kevin): 调整方法名

--- app/index/controller/Industry.php
+++ app/index/controller/Industry.php
@@ -33,10 +33,24 @@ class Industry extends BaseController
     }
 
     /**
-     * 文章列表
+     * 文章内容
      * @return string
      */
     final public function index(): string
+    {
+        $info = $this->services->get($this->id, null, ['content']);
+        // 阅读量自增
+        $info && $this->services->setInc($info['id'], $this->incValue);
+        // 上/下一篇文章
+        $prenext = $this->services->getPrenext($info['id'], null, 'id, cid, title');
+        return $this->view::fetch('../industry/index', compact('info', 'prenext'));
+    }
+
+    /**
+     * 文章列表
+     * @return string
+     */
+    final public function list(): string
     {
         $name = ['name' => getPath()];
         $pid = $this->channelServices->value($name, 'pid');
@@ -45,21 +59,7 @@ class Industry extends BaseController
             ? $this->status
             : array_merge($this->status, ['cid' => $this->channelServices->value($name)]);
         $list = $this->services->getPaginate($map, $this->pageSize, $this->field, $this->order, ['channel']);
-        return $this->view::fetch('../industry/index', compact('list'));
-    }
-
-    /**
-     * 文章内容
-     * @return string
-     */
-    final public function detail(): string
-    {
-        $result = $this->services->get($this->id, null, ['content']);
-        // 阅读量自增
-        $result && $this->services->setInc($result['id'], $this->incValue);
-        // 上/下一篇文章
-        $prenext = $this->services->getPrenext($result['id'], 'id, cid, title');
-        return $this->view::fetch('../industry/detail', ['result' => $result, 'prenext' => $prenext]);
+        return $this->view::fetch('../industry/list', compact('list'));
     }
 
     /**
@@ -72,7 +72,7 @@ class Industry extends BaseController
             $this->current,
             $this->pageSize,
             $this->status,
-            'id, click, title, litpic, create_time',
+            'id, cid, click, title, litpic, create_time',
             ['click' => 'desc'], null, null, ['channel']);
     }
 }
