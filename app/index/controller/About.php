6dfe8ce72e1224395516f44ddd6b4d73d5eb8405
Kevin <wcdma020@hotmail.com>
2022-09-23 00:26:55 +0800

    chore(Kevin): 删除冗余方法

--- app/index/controller/About.php
+++ app/index/controller/About.php
@@ -10,14 +10,4 @@ class About extends BaseController
     {
         return $this->view::fetch('../about/index');
     }
-
-    final public function system(): string
-    {
-        return $this->view::fetch('../about/system');
-    }
-
-    final public function produce(): string
-    {
-        return $this->view::fetch('../about/produce');
-    }
 }
