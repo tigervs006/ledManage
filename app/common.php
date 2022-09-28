e1bde79eed27fd5d55b6938ace7cc0314debcf13
Kevin <wcdma020@hotmail.com>
2022-09-28 10:37:11 +0800

    chore(Kevin): 新增formatBytes格式化字节大小方法

--- app/common.php
+++ app/common.php
@@ -30,6 +30,21 @@ if (!function_exists('msectime')) {
     }
 }
 
+if (!function_exists('formatBytes')) {
+    /**
+     * 格式化字节大小
+     * @return string
+     * @param  number $size 字节数
+     * @param  string $delimiter 数字和单位分隔符
+     */
+    function formatBytes($size, string $delimiter = ''): string
+    {
+        $units = array('B', 'KB', 'MB', 'GB', 'TB', 'PB');
+        for ($i = 0; $size >= 1024 && $i < 5; $i++) $size /= 1024;
+        return round($size, 2) . $delimiter . $units[$i];
+    }
+}
+
 if (!function_exists('strOrderFilter')) {
     /**
      * 排序字段字符串截取
